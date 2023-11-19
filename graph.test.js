63a45aeeeffc16c84b3746d79511fd3c1d17d12c
Alexandre Becker <alerkay@melix.org>
2023-11-19 20:57:14 +0100

    graph.test.js: rewrite tests for getUniqueElements
    With actual object equality

--- graph.test.js
+++ graph.test.js
@@ -1,33 +1,35 @@
 const getUniqueElements = require('./graph');
 
-test('getUniqueElements 1', () => {
-  expect(getUniqueElements([[1,2]]).size).toBe(2);
-});
+describe('getUniqueElements tests', () => {
+    test('should return an empty set for empty input', () => {
+        expect(getUniqueElements([])).toEqual(new Set());
+    });
 
-test('getUniqueElements 1', () => {
-  expect(getUniqueElements([[1,2], [1,2]]).size).toBe(2);
-});
+    test('should handle a single pair', () => {
+        expect(getUniqueElements([[1, 2]])).toEqual(new Set([1, 2]));
+    });
 
-test('getUniqueElements 1', () => {
-  expect(getUniqueElements([]).size).toBe(0);
-});
+    test('should handle multiple pairs with unique elements', () => {
+        expect(getUniqueElements([[1, 2], [3, 4]])).toEqual(new Set([1, 2, 3, 4]));
+    });
 
-test('getUniqueElements 1', () => {
-  expect(getUniqueElements([[1,2], [2,3]]).size).toBe(3);
-});
+    test('should handle multiple pairs with unique elements', () => {
+        expect(getUniqueElements([[1, 2], [3, 4], [5, 6]])).toEqual(new Set([1, 2, 3, 4, 5, 6]));
+    });
 
-test('getUniqueElements 1', () => {
-  expect(getUniqueElements([[1,2], [3,2]]).size).toBe(3);
-});
+    test('should handle multiple pairs with duplicate elements', () => {
+        expect(getUniqueElements([[1, 2], [1, 2]])).toEqual(new Set([1, 2]));
+    });
 
-test('getUniqueElements 1', () => {
-  expect(getUniqueElements([[1,2], [3,4]]).size).toBe(4);
-});
+    test('should handle multiple pairs with duplicate elements', () => {
+        expect(getUniqueElements([[1, 2], [2, 3]])).toEqual(new Set([1, 2, 3]));
+    });
 
-test('getUniqueElements 1', () => {
-  expect(getUniqueElements([[1,2], [3,4], [5,6]]).size).toBe(6);
-});
+    test('should handle multiple pairs with duplicate elements', () => {
+        expect(getUniqueElements([[1, 2], [2, 3], [3, 1]])).toEqual(new Set([1, 2, 3]));
+    });
 
-test('getUniqueElements 1', () => {
-  expect(getUniqueElements([[1,2], [2,5], [5,6]]).size).toBe(4);
+    test('should handle multiple pairs with duplicate elements', () => {
+        expect(getUniqueElements([[1, 2], [2, 5], [5, 6]])).toEqual(new Set([1, 2, 5, 6]));
+    });
 });
