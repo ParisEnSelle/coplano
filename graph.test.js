1bb8f1914c622d95ec34088334c2952bf609fb22
Alexandre Becker <alerkay@melix.org>
2024-02-19 22:29:10 +0100

    graph.test.js: add more tests

--- graph.test.js
+++ graph.test.js
@@ -33,3 +33,77 @@ describe('getUniqueElements tests', () => {
         expect(getUniqueElements([[1, 2], [2, 5], [5, 6]])).toEqual(new Set([1, 2, 5, 6]));
     });
 });
+
+
+
+describe('depthFirstSearch tests', () => {
+    test('Empty Graph', () => {
+        const graph = {};
+        const labels = new Set();
+        expect(depthFirstSearch(graph, 1, labels)).toEqual([]);
+    });
+
+
+    test('Single Node Graph Without Destination', () => {
+        const graph = { 1: [] };
+        const labels = new Set([2]);
+        expect(depthFirstSearch(graph, 1, labels)).toEqual([]);
+    });
+
+    test('Graph With Multiple Paths', () => {
+        const graph = {
+            1: [2, 3],
+            2: [4],
+            3: [4],
+            4: []
+        };
+        const labels = new Set([4]);
+        expect(depthFirstSearch(graph, 1, labels)).toEqual([[1, 2, 4], [1, 3, 4]]);
+    });
+
+    test('Graph With Cycles', () => {
+        const graph = {
+            1: [2],
+            2: [1, 3],
+            3: [2]
+        };
+        const labels = new Set([3]);
+        expect(depthFirstSearch(graph, 1, labels)).toEqual([[1, 2, 3]]);
+    });
+
+    test('Graph With Complex Structure', () => {
+        const graph = {
+            1: [2, 5],
+            2: [3],
+            3: [4],
+            4: [],
+            5: [6],
+            6: [4]
+        };
+        const labels = new Set([4]);
+        expect(depthFirstSearch(graph, 1, labels)).toEqual([[1, 2, 3, 4], [1, 5, 6, 4]]);
+    });
+
+    test('Destination Node Not In Graph', () => {
+        const graph = {
+            1: [2],
+            2: [3],
+            3: []
+        };
+        const labels = new Set([3]);
+        expect(depthFirstSearch(graph, 4, labels)).toEqual([]);
+    });
+});
+
+describe('getRatRuns tests', () => {
+    test('Transit Sets Only', () => {
+        const graph = {
+            1: [2, 3],
+            2: [4],
+            3: [4],
+            4: []
+        };
+        const transitSets = [new Set([1]), new Set([4])];
+        expect(getRatRuns(graph, transitSets)).toEqual([[1, 2, 4], [1, 3, 4]]);
+    });
+});
