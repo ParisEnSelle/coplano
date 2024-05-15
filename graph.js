db805bf77cdbccf8cce7f6f753e676d27b6d82f1
Alexandre Becker <alerkay@melix.org>
2024-05-15 20:58:34 +0200

    map, graph.js: extract graph building functions into graph.js
    Note: plan to move the graph building functions into a distinct file
    when I can handle browser and node imports properly

--- graph.js
+++ graph.js
@@ -3,6 +3,263 @@ if (typeof LOG_LEVEL === 'undefined') {
     LOG_LEVEL = 1;
 }
 
+//
+// Graph building functions
+//
+
+// Build list of points
+class Point {
+    constructor(lat, long, id) {
+        this.lat = lat;
+        this.long = long;
+        this.id = parseInt(id); // for retro-compatibility, should remove it
+        this.neighbors = {};
+    }
+}
+
+
+const Direction = {
+    BASE: 'base',
+    REVERSE: 'reverse',
+    DOUBLE: 'double',
+    NONE: 'none'
+};
+
+
+let startEnds;
+let nb_nodes = 0;
+let nb_segments = 0;
+
+// Verify there is no duplicate  a->b b->a neighbor relationship
+function checkNoSelfReferencingNeighbors(points) {
+    let logs = "";
+    for (let id in points) {
+        let neighbors = points[id].neighbors;
+        for (let n in neighbors) {
+            if (points[n] && points[n].neighbors && points[n].neighbors[id] && id < n) { // Only display message once
+                output = `Error: points ${id} and ${n} are self-referencing each other`
+                console.log(output);
+                logs += output + "\n";
+            }
+        }
+    }
+    return logs;
+}
+
+function checkIdValid(points) {
+    let logs = "";
+    for (let p in points) {
+        let id = points[p].id;
+        if (!Number.isInteger(id) || id < 0) { // 0 is special value authorized
+            let output = `Error: a point has an invalid id ${id}`
+            console.log(output);
+            logs += output + "\n";
+        }
+    }
+    return logs;
+}
+
+function checkPointErrors(points) {
+    let logs = checkNoSelfReferencingNeighbors(points);
+    logs += checkIdValid(points);
+    if (logs) {
+        alert("Error(s) on importing geojson, see details below. Please cleanup the geojson and reload the file.\n\n" + logs);
+        throw("Geojson point configuration error");
+    }
+}
+
+function describePoints(points) {
+    let nodes = 0;
+    let segments = 0;
+    for (let id in points) {
+        nodes += 1;
+        for (let n in points[id].neighbors) {
+            segments += 1;
+        }
+    }
+    nb_nodes = nodes;
+    nb_segments = segments;
+    if (LOG_LEVEL >= 1) {
+        console.log(`Initialization: loaded ${nodes} nodes with ${segments} segments.`)
+    }
+}
+
+
+function parsePoints(points) {
+    //console.log("parsing points:", points.length)
+    let pointsDictionary = {};
+    for (let point of points) {
+        let newPoint = new Point(point.geometry.coordinates[1], point.geometry.coordinates[0], point.properties.id);
+        function parseNeighbors(list, direction) {
+            if (list) {
+                neighborsList = Array.from(list.split(","), Number);
+                neighborsList.forEach((n, i, a) => newPoint.neighbors[n] = direction);
+            }
+        }
+        parseNeighbors(point.properties.local_street , Direction.BASE);
+        parseNeighbors(point.properties.local_street_double_way , Direction.DOUBLE);
+        parseNeighbors(point.properties.local_street_modal_filter , Direction.NONE);
+        newPoint.transit = point.properties.transit_node && point.properties.transit_node == "1";
+        if (point.properties.transit_street) {
+            newPoint.neighbors_transit = Array.from(point.properties.transit_street.split(","), Number);
+        }
+        // Keep retro-compatibility with transit exceptions
+        // 'transit exceptions' have been replaced with 'transit blacklist'
+        if (point.properties.transit_exceptions) {
+            newPoint.transit_blacklist = Array.from(point.properties.transit_exceptions.split(","), Number);
+        }
+        if (point.properties.transit_blacklist) {
+            newPoint.transit_blacklist = Array.from(point.properties.transit_blacklist.split(","), Number);
+        }
+        if (point.properties.transit_whitelist) {
+            newPoint.transit_whitelist = Array.from(point.properties.transit_whitelist.split(","), Number);
+        }
+        pointsDictionary[point.properties.id] = newPoint;
+    }
+    return pointsDictionary;
+}
+
+
+function buildGraphfromPoints(points) {
+    let pairs = [];
+    for (let start in points) {
+        start = parseInt(start);
+        let p = points[start];
+        for (let end in p.neighbors) {
+            end = parseInt(end);
+            let direction = p.neighbors[end];
+            if (direction === Direction.BASE) {
+                pairs.push([start, end]);
+            } else if (direction === Direction.REVERSE) {
+                pairs.push([end, start]);
+            } else if (direction === Direction.DOUBLE) {
+                pairs.push([start, end]);
+                pairs.push([end, start]);
+            }
+        }
+    }
+    return buildGraphfromPairs(pairs);
+}
+
+
+function getTransitSets(transitGraph) {
+    let assignedTransit = {};
+    let transitStreets = {};
+    let transitCounter = 0;
+
+    for (let key in transitGraph) {
+        // account for cases where no transit neighbors was defined for a transit node
+        if (transitGraph[key] === undefined) {
+            transitGraph[key] = [Number(key)];
+        }
+        for (let n of transitGraph[key]) {
+            let transitId;
+            if (!assignedTransit[key] && !assignedTransit[n]) {
+                transitCounter += 1;
+                transitId = transitCounter;
+                transitStreets[transitId] = [];
+            } else if (assignedTransit[key] && assignedTransit[n]) {
+                transitId = assignedTransit[key];
+                let transitIdOther = assignedTransit[n];
+                if (transitId != transitIdOther) {  // merge two transit sets
+                    let transitStreetOther = transitStreets[transitIdOther];
+                    let transitStreetOtherNodes = getUniqueElements(transitStreetOther);
+                    for (let i of transitStreetOtherNodes) {
+                      assignedTransit[i] = transitId;
+                    }
+                    transitStreets[transitId] = transitStreets[transitId].concat(transitStreetOther);
+                    delete transitStreets[transitIdOther]; //remove item from dico
+                }
+            } else if (assignedTransit[n]) {
+                transitId = assignedTransit[n];
+            } else if (assignedTransit[key]) {
+                transitId = assignedTransit[key];
+            }
+            assignedTransit[key] = transitId;
+            assignedTransit[n] = transitId;
+            transitStreets[transitId].push([Number(key), n]);
+        }
+    }
+
+
+    for (let key in transitStreets) {
+        transitStreets[key] =  getUniqueElements(transitStreets[key]);
+    }
+    // TODO: verify all nodes marked as transit are part of a transit street
+    return Object.values(transitStreets);
+}
+
+function buildTransitStreets(points) {
+    let transitGraph = {};
+    for (let p in points) {
+        if (points[p].transit) {
+            transitGraph[p] = points[p].neighbors_transit;
+            if (LOG_LEVEL >= 2) {
+                console.log(`Transit node ${p}: ${points[p].neighbors_transit}`)
+            }
+        }
+    }
+
+    return getTransitSets(transitGraph);
+}
+
+function buildTransitBlacklists(points) {
+    let transitBlacklists = {};
+    for (let p in points) {
+        if (points[p].transit_blacklist) {
+            transitBlacklists[p] = points[p].transit_blacklist;
+            if (LOG_LEVEL >= 2) {
+                console.log(`Transit blacklist from ${p}: ${points[p].transit_blacklist}`)
+            }
+        }
+    }
+
+    return transitBlacklists;
+}
+
+function buildTransitWhitelists(points) {
+    let transitWhitelists = {};
+    for (let p in points) {
+        if (points[p].transit_whitelist) {
+            transitWhitelists[p] = points[p].transit_whitelist;
+            if (LOG_LEVEL >= 2) {
+                console.log(`Transit whitelist from ${p}: ${points[p].transit_whitelist}`)
+            }
+        }
+    }
+
+    return transitWhitelists;
+}
+
+function checkRatRunSettings(points) {
+    let processRatRuns = true;
+    for (let point of points) {
+        if (point.properties.ignore_rat_runs) {
+            console.log("Skipping rat runs");
+            processRatRuns = false;
+        }
+    }
+    return processRatRuns;
+}
+
+
+function getPlanObjects(geojson) {
+    let dict = parsePoints(geojson.features);
+    checkPointErrors(dict);
+    describePoints(dict);
+    let transitSets = buildTransitStreets(dict);
+    let transitBlacklists = buildTransitBlacklists(dict);
+    let transitWhitelists = buildTransitWhitelists(dict);
+    startEnds = getStartEnds(transitSets, transitBlacklists, transitWhitelists);
+    let graph = buildGraphfromPoints(dict);
+    let processRatRuns = checkRatRunSettings(geojson.features);
+    return { points: dict, graph, startEnds, processRatRuns };
+}
+
+//
+// Graph computation functions
+//
+
 // Utility functions
 function displayText(sentence) {
     let p = document.createElement("p");
--- map.js
+++ map.js
@@ -1,23 +1,6 @@
-// Build list of points
-class Point {
-    constructor(lat, long, id) {
-        this.lat = lat;
-        this.long = long;
-        this.id = parseInt(id); // for retro-compatibility, should remove it
-        this.neighbors = {};
-    }
-}
-
 const Constant = {
     MODAL_FILTER_DASH: '10,10'
 }
-
-const Direction = {
-    BASE: 'base',
-    REVERSE: 'reverse',
-    DOUBLE: 'double',
-    NONE: 'none'
-};
 
 const arrowSettings = {
     size: '15px',
@@ -30,10 +13,7 @@ let LOG_LEVEL = 0;
 
 let streets = L.featureGroup();
 let streets_rr = L.featureGroup();
-let startEnds;
 let processRatRuns = true;
-let nb_nodes = 0;
-let nb_segments = 0;
 
 // Create the map
 var map = L.map('map', { doubleClickZoom: false }).setView([48.89, 2.345], 15); // disable double-click zoom to avoid confusion when clicking arrows
@@ -43,93 +23,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Verify there is no duplicate  a->b b->a neighbor relationship
-function checkNoSelfReferencingNeighbors(points) {
-    let logs = "";
-    for (let id in points) {
-        let neighbors = points[id].neighbors;
-        for (let n in neighbors) {
-            if (points[n] && points[n].neighbors && points[n].neighbors[id] && id < n) { // Only display message once
-                output = `Error: points ${id} and ${n} are self-referencing each other`
-                console.log(output);
-                logs += output + "\n";
-            }
-        }
-    }
-    return logs;
-}
-
-function checkIdValid(points) {
-    let logs = "";
-    for (let p in points) {
-        let id = points[p].id;
-        if (!Number.isInteger(id) || id < 0) { // 0 is special value authorized
-            let output = `Error: a point has an invalid id ${id}`
-            console.log(output);
-            logs += output + "\n";
-        }
-    }
-    return logs;
-}
-
-function checkPointErrors(points) {
-    let logs = checkNoSelfReferencingNeighbors(points);
-    logs += checkIdValid(points);
-    if (logs) {
-        alert("Error(s) on importing geojson, see details below. Please cleanup the geojson and reload the file.\n\n" + logs);
-        throw("Geojson point configuration error");
-    }
-}
-
-function describePoints(points) {
-    let nodes = 0;
-    let segments = 0;
-    for (let id in points) {
-        nodes += 1;
-        for (let n in points[id].neighbors) {
-            segments += 1;
-        }
-    }
-    nb_nodes = nodes;
-    nb_segments = segments;
-    if (LOG_LEVEL >= 1) {
-        console.log(`Initialization: loaded ${nodes} nodes with ${segments} segments.`)
-    }
-}
-
-function parsePoints(points) {
-    //console.log("parsing points:", points.length)
-    let pointsDictionary = {};
-    for (let point of points) {
-        let newPoint = new Point(point.geometry.coordinates[1], point.geometry.coordinates[0], point.properties.id);
-        function parseNeighbors(list, direction) {
-            if (list) {
-                neighborsList = Array.from(list.split(","), Number);
-                neighborsList.forEach((n, i, a) => newPoint.neighbors[n] = direction);
-            }
-        }
-        parseNeighbors(point.properties.local_street , Direction.BASE);
-        parseNeighbors(point.properties.local_street_double_way , Direction.DOUBLE);
-        parseNeighbors(point.properties.local_street_modal_filter , Direction.NONE);
-        newPoint.transit = point.properties.transit_node && point.properties.transit_node == "1";
-        if (point.properties.transit_street) {
-            newPoint.neighbors_transit = Array.from(point.properties.transit_street.split(","), Number);
-        }
-        // Keep retro-compatibility with transit exceptions
-        // 'transit exceptions' have been replaced with 'transit blacklist'
-        if (point.properties.transit_exceptions) {
-            newPoint.transit_blacklist = Array.from(point.properties.transit_exceptions.split(","), Number);
-        }
-        if (point.properties.transit_blacklist) {
-            newPoint.transit_blacklist = Array.from(point.properties.transit_blacklist.split(","), Number);
-        }
-        if (point.properties.transit_whitelist) {
-            newPoint.transit_whitelist = Array.from(point.properties.transit_whitelist.split(","), Number);
-        }
-        pointsDictionary[point.properties.id] = newPoint;
-    }
-    return pointsDictionary;
-}
 
 function addDoubleArrow(polyline, arrowColor) {
     var polyline2 = L.polyline(polyline.getLatLngs(), {color: arrowColor, interactive: false }).arrowheads(arrowSettings);
@@ -180,116 +73,6 @@ function reverseArrow(ev) {
 
     refreshRatRuns();
     displayRatRuns();
-}
-
-function getTransitSets(transitGraph) {
-    let assignedTransit = {};
-    let transitStreets = {};
-    let transitCounter = 0;
-
-    for (let key in transitGraph) {
-        // account for cases where no transit neighbors was defined for a transit node
-        if (transitGraph[key] === undefined) {
-            transitGraph[key] = [Number(key)];
-        }
-        for (let n of transitGraph[key]) {
-            let transitId;
-            if (!assignedTransit[key] && !assignedTransit[n]) {
-                transitCounter += 1;
-                transitId = transitCounter;
-                transitStreets[transitId] = [];
-            } else if (assignedTransit[key] && assignedTransit[n]) {
-                transitId = assignedTransit[key];
-                let transitIdOther = assignedTransit[n];
-                if (transitId != transitIdOther) {  // merge two transit sets
-                    let transitStreetOther = transitStreets[transitIdOther];
-                    let transitStreetOtherNodes = getUniqueElements(transitStreetOther);
-                    for (let i of transitStreetOtherNodes) {
-                      assignedTransit[i] = transitId;
-                    }
-                    transitStreets[transitId] = transitStreets[transitId].concat(transitStreetOther);
-                    delete transitStreets[transitIdOther]; //remove item from dico
-                }
-            } else if (assignedTransit[n]) {
-                transitId = assignedTransit[n];
-            } else if (assignedTransit[key]) {
-                transitId = assignedTransit[key];
-            }
-            assignedTransit[key] = transitId;
-            assignedTransit[n] = transitId;
-            transitStreets[transitId].push([Number(key), n]);
-        }
-    }
-
-
-    for (let key in transitStreets) {
-        transitStreets[key] =  getUniqueElements(transitStreets[key]);
-    }
-    // TODO: verify all nodes marked as transit are part of a transit street
-    return Object.values(transitStreets);
-}
-
-function buildTransitStreets(points) {
-    let transitGraph = {};
-    for (let p in points) {
-        if (points[p].transit) {
-            transitGraph[p] = points[p].neighbors_transit;
-            if (LOG_LEVEL >= 2) {
-                console.log(`Transit node ${p}: ${points[p].neighbors_transit}`)
-            }
-        }
-    }
-
-    return getTransitSets(transitGraph);
-}
-
-function buildTransitBlacklists(points) {
-    let transitBlacklists = {};
-    for (let p in points) {
-        if (points[p].transit_blacklist) {
-            transitBlacklists[p] = points[p].transit_blacklist;
-            if (LOG_LEVEL >= 2) {
-                console.log(`Transit blacklist from ${p}: ${points[p].transit_blacklist}`)
-            }
-        }
-    }
-
-    return transitBlacklists;
-}
-
-function buildTransitWhitelists(points) {
-    let transitWhitelists = {};
-    for (let p in points) {
-        if (points[p].transit_whitelist) {
-            transitWhitelists[p] = points[p].transit_whitelist;
-            if (LOG_LEVEL >= 2) {
-                console.log(`Transit whitelist from ${p}: ${points[p].transit_whitelist}`)
-            }
-        }
-    }
-
-    return transitWhitelists;
-}
-
-function buildGraphfromPoints(points) {
-    let pairs = [];
-    for (let start in points) {
-        start = parseInt(start);
-        let p = points[start];
-        for (let end in p.neighbors) {
-            end = parseInt(end);
-            let direction = p.neighbors[end];
-            if (direction === Direction.BASE) {
-                pairs.push([start, end]);
-            } else if (direction === Direction.REVERSE) {
-                pairs.push([end, start]);
-            } else if (direction === Direction.DOUBLE) {
-                pairs.push([start, end]);
-                pairs.push([end, start]);
-            }
-        }
-    }
-    return buildGraphfromPairs(pairs);
 }
 
 function buildGraph(polylineLayerGroup) {
@@ -398,30 +181,6 @@ function drawStreets(pointDictionary) {
         }
     }
     streets.addTo(map);
-}
-
-function getPlanObjects(geojson) {
-    let dict = parsePoints(geojson.features);
-    checkPointErrors(dict);
-    describePoints(dict);
-    let transitSets = buildTransitStreets(dict);
-    let transitBlacklists = buildTransitBlacklists(dict);
-    let transitWhitelists = buildTransitWhitelists(dict);
-    startEnds = getStartEnds(transitSets, transitBlacklists, transitWhitelists);
-    let graph = buildGraphfromPoints(dict);
-    let processRatRuns = checkRatRunSettings(dict);
-    return { points: dict, graph, startEnds, processRatRuns };
-}
-
-function checkRatRunSettings(points) {
-    let processRatRuns = true;
-    for (let point of points) {
-        if (point.properties.ignore_rat_runs) {
-            console.log("Skipping rat runs");
-            processRatRuns = false;
-        }
-    }
-    return processRatRuns;
 }
 
 function processGeojson(geojson) {
